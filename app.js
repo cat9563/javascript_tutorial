4eb47835b49fe3d51934f6e0178b511b55b023ca
cat9563 <crysatl9563@gmail.com>
2019-02-16 15:37:43 -0500

    deletes tasks, clears tasks, and filters tasks,

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('.clear-task');
+const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
@@ -8,7 +8,14 @@ const taskInput = document.querySelector('#task');
 loadEventListeners();
 
 function loadEventListeners() {
+    //Add task event
     form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+    // Clear tasks
+    clearBtn.addEventListener('click', clearTasks);
+    //filter tasks
+    filter.addEventListener('keyup', filterTasks);
 }
 
 //Add Task
@@ -36,4 +43,36 @@ function addTask(e){
     //clear input
     taskInput.value = '';
     e.preventDefault();
+}
+
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Are you sure?'))
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+function clearTasks(){
+    //two ways of doing this the first way 
+    // taskList.innerHTML = '';
+    //https://jsperf.com/innerhtml-vs-removechild
+
+    //faster way
+   while(taskList.firstChild){
+       taskList.removeChild(taskList.firstChild);
+   }
+}
+
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach
+    (function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) !== -1){
+            task.style.display = 'block';
+        }else {
+            task.style.display = 'none';
+        }
+    });
 }
